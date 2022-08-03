972f308b67ab104e3ec507c7615d404d25ea8870
silentknight2001 <silentknight.www@gmail.com>
2022-08-03 18:45:39 +0530

    navbar first commit

---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import React, {useState, useRef, useEffect} from 'react'
+import {FaBars} from 'react-icons/fa';
+import {links, social} from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+const [showLinks, setShowLinks] = useState(false);
+const linksContainerRef = useRef(null);
+const linksRef = useRef(null);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo"/>
+          <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}>
+            <FaBars/>
+          </button>
+        </div>
+
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+           {links.map((link)=>{
+             const {id, url, text} = link;
+             return <li key={id}>
+               <a href={url}>{text}</a>
+             </li>
+           })}
+          </ul>
+        </div>
+
+        <ul className="social-icons">
+          {social.map((socialIcon)=>{
+            const {id, url, icon} = socialIcon;
+            return <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
